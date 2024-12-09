92039c2478c5cc87952e55043d0fd74f5aaa4d10
Arsen <arsen.zuna01@gmail.com>
2024-12-09 05:22:28 +0100

    firebase fix

--- src/utils/firebaseConfig.jsx
+++ src/utils/firebaseConfig.jsx
@@ -7,7 +7,7 @@ const firebaseConfig = {
 	authDomain: "auctionapp-project.firebaseapp.com",
 	databaseURL: "https://auctionapp-project-default-rtdb.firebaseio.com",
 	projectId: "auctionapp-project",
-	storageBucket: "auctionapp-project.appspot.com",
+	storageBucket: "auctionapp-project.firebasestorage.app",
 	messagingSenderId: "853754778238",
 	appId: "1:853754778238:web:5b4382d7b0495d3dbd7e2f",
 	measurementId: "G-ZMMP3N433W"
